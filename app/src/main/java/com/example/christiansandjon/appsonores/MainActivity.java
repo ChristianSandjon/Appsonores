7ea49e6d99ba8b0cc4495e5d66aa454157ecb788
ChristianSandjon <christiansandjong@hotmail.com>
2018-09-25 14:52:27 +0200

    page d'accueil faite + acces aux details parcelable

--- app/src/main/java/com/example/christiansandjon/appsonores/MainActivity.java
+++ app/src/main/java/com/example/christiansandjon/appsonores/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.christiansandjon.appsonores;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.Spinner;
@@ -14,7 +15,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private SearchView searchView;
-    private Spinner spinner1, spinner2;
+    private Spinner spinner_level, spinner_decibel;
     private ListView listView;
     private ArrayList<DataModel> dataModels;
     private SonorAdapter adapter;
@@ -24,13 +25,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        spinner_decibel = findViewById(R.id.spinner_decibel);
+        spinner_level = findViewById(R.id.spinner_level);
+        listView=findViewById(R.id.list_sonore);
 
-        listView=(ListView)findViewById(R.id.list_sonore);
+        //spinner couleur
+        String[] colors = new String[]{"Vert", "Orange", "Rouge"};
+        ArrayAdapter<String> adapterColors = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, colors);
+        spinner_level.setAdapter(adapterColors);
 
+        //spinner decibel
+        String[] decibels = new String[]{"0", "10","20", "30","40", "50","60", "70","80", "90","100", "110","120"};
+        ArrayAdapter<String> adapterDecibel = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, decibels);
+        spinner_decibel.setAdapter(adapterDecibel);
+
+        // data
         dataModels= new ArrayList<>();
 
         dataModels.add(new DataModel("Bruxelles", "bf formation", "20/09/2018",20.4));
-
+        dataModels.add(new DataModel("Anvers", "bf formation", "20/09/2018",20.4));
+        dataModels.add(new DataModel("Liege", "bf formation", "20/09/2018",20.4));
+        dataModels.add(new DataModel("Bruxelles", "bf formation", "20/09/2018",20.4));
+        dataModels.add(new DataModel("Anvers", "bf formation", "20/09/2018",20.4));
+        dataModels.add(new DataModel("Liege", "bf formation", "20/09/2018",20.4));
+        dataModels.add(new DataModel("Bruxelles", "bf formation", "20/09/2018",20.4));
+        dataModels.add(new DataModel("Anvers", "bf formation", "20/09/2018",20.4));
+        dataModels.add(new DataModel("Liege", "bf formation", "20/09/2018",20.4));
+        dataModels.add(new DataModel("Bruxelles", "bf formation", "20/09/2018",20.4));
+        dataModels.add(new DataModel("Anvers", "bf formation", "20/09/2018",20.4));
+        dataModels.add(new DataModel("Liege", "bf formation", "20/09/2018",20.4));
         adapter= new SonorAdapter(dataModels,getApplicationContext());
 
         listView.setAdapter(adapter);
